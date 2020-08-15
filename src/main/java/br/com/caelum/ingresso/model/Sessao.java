be9748408a22a26108022e49954195aa92308e05
Anderson Napoleão de Mello <anmello@latam.stefanini.com>
2020-08-15 17:16:25 -0300

    adicionando preco

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -24,6 +26,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	
+	private BigDecimal preco;
+	
 	public Sessao() {
 		
 	}
@@ -32,6 +36,7 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -64,6 +69,10 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+	
+	public BigDecimal getPreco() {
+		return this.preco.setScale(2,RoundingMode.HALF_UP);
 	}
 	
 }
